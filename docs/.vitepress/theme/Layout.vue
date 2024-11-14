8d714fadb3bdaec8cdd04bee55b338c90b206914
cy <chanwit.yimneam@hotmail.com>
2024-11-14 17:35:25 +0700

    Refactor Layout.vue: update doc-top template structure and adjust styles for improved layout

--- docs/.vitepress/theme/Layout.vue
+++ docs/.vitepress/theme/Layout.vue
@@ -23,13 +23,16 @@ const { site, frontmatter } = useData()
   <div v-else>
     <!-- <a href="/">Home</a> -->
     <DefaultTheme.Layout>
+
       <template #doc-top>
-        <div
-          class="absolute flex flex-col z-[40] w-full !max-w-full items-center justify-center bg-transparent transition-bg overflow-hidden h-screen">
-          <div class="jumbo absolute opacity-60" />
+        <!-- <div class="h-[220px] top-0 left-0 opacity-25 dark:opacity-[.55] pointer-events-none"> -->
+          <div
+            class="absolute flex flex-col z-[40] w-full !max-w-full items-center justify-center bg-transparent transition-bg overflow-hidden h-[220px] top-0 left-0 opacity-25 dark:opacity-[.55] pointer-events-none">
+            <div class="jumbo absolute opacity-60" />
+          <!-- </div> -->
         </div>
-        <Content />
-        <div id="box" class="absolute top-10 left-10 w-5 h-5 bg-slate-50" />
+        <!-- <div id="box" class="absolute top-10 left-10 w-5 h-5 bg-slate-50" /> -->
+        <!-- <Content /> -->
       </template>
     </DefaultTheme.Layout>
   </div>
